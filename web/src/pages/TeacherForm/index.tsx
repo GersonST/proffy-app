6e011644226445e1cea337f7404bf1ff73c3636d
GersonST <gersonsevero90@gmail.com>
2021-08-16 08:56:48 -0300

    Install Router-Dom

---
+++ web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function TeacherForm() {
+  return (
+    <div>
+      <h1>Teacher Form</h1>
+    </div>
+  );
+}
---
+++ web/src/pages/TeacherList/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function TeacherList() {
+  return (
+    <div>
+      <h1>Teacher List</h1>
+    </div>
+  );
+}
