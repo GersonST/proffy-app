ea4bd629648b31b8a45acb852fe2c85501c6a129
GersonST <gersonsevero90@gmail.com>
2021-08-22 10:36:18 -0300

    Banco de dados criado

---
+++ server/src/database/migrations/01_create_classes.ts
@@ -0,0 +1,23 @@
+import { Knex } from "knex";
+
+export async function up(knex: Knex) {
+  return knex.schema.createTable("classes", (table) => {
+    table.increments("id").primary();
+    table.string("subject").notNullable();
+    table.decimal("cost").notNullable();
+
+    //relacionamento da tabela com o usuario ou professor de acordo com esses dados, essa eh a forma de criar relacionamentos utilizando o knex
+
+    table
+      .integer("user_id")
+      .notNullable()
+      .references("id")
+      .inTable("users")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+  });
+}
+
+export async function down(knex: Knex) {
+  return knex.schema.dropTable("classes");
+}
